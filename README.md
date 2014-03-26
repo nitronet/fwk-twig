72dfb413a82e135233252572ecfdf929d1d3220e
Julien <julien@nitronet.org>
2014-03-26 01:50:39 +0100

    started dev

---
+++ README.md
@@ -0,0 +1,63 @@
+# fwk-twig
+
+Brings [Twig](http://twig.sensiolabs.org/) template engine support for [Fwk\Core](https://github.com/fwk/Core) Applications.
+
+## Installation
+
+### 1: Install the sources
+
+Via [Composer](http://getcomposer.org):
+
+```
+{
+    "require": {
+        "fwk/fwk-twig": "dev-master",
+    }
+}
+```
+
+If you don't use Composer, you can still [download](https://github.com/fwk/fwk-twig/zipball/master) this repository and add it
+to your ```include_path``` [PSR-0 compatible](https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md)
+
+### 2: Defines Twig as a Service
+
+Define a ```Twig_Environment``` instance in your [Di](https://github.com/fwk/Di) Container, and configure it the way you want:
+
+``` xml
+<!-- Twig_Loader definition -->
+<class-definition name="twig.loader" class="\Twig_Loader_Filesystem" shared="true">
+    <argument>/path/to/templates</argument>
+</class-definition>
+
+<!-- Twig_Environment definition -->
+<class-definition name="twig" class="\Twig_Environment" shared="true">
+    <argument>@twig.loader</argument>
+    <argument type="json">{debug: false, cache: "/path/to/compilation_cache"}</argument>
+</class-definition>
+```
+
+### 3: Register Twig ResultType
+
+Registers a new ResultType in fwk.xml:
+
+``` xml
+<result-type name="twig" class="FwkTwig\TwigResultType" />
+```
+
+### 4: Enjoy!
+
+Use the ResultType where you want:
+
+``` xml
+<action name="Home" shortcut="MyApp\Controllers\Home:show">
+    <result name="success" type="twig">
+        <param name="file">home.twig</param>
+    </result>
+</action>
+```
+
+## Contributions / Community
+
+- Issues on Github: https://github.com/fwk/fwk-twig/issues
+- Follow *Fwk* on Twitter: [@phpfwk](https://twitter.com/phpfwk)
+
---
+++ src/FwkTwig/TwigResultType.php
@@ -0,0 +1,71 @@
+<?php
+namespace FwkTwig;
+
+use Fwk\Core\Components\ResultType\ResultType;
+use Symfony\Component\HttpFoundation\Response;
+use Fwk\Core\ServicesAware;
+use Fwk\Di\Container;
+
+/**
+ * Adds Twig templates support to Fwk/Core
+ * @see Fwk\Core\Components\ResultType\ResultType
+ * 
+ * @category ResultTypes
+ * @author   Julien Ballestracci
+ */
+class TwigResultType implements ResultType, ServicesAware
+{
+    protected $services;
+    protected $params = array();
+    
+    /**
+     * Constructor
+     * 
+     * @param array $params Twig configuration options
+     * 
+     * @return void
+     */
+    public function __construct(array $params = array())
+    {
+        $this->params = array_merge(
+            array(
+                'twigService' => "twig"
+            ),
+            $params
+        );
+    }
+    
+    /**
+     * Renders a twig template and returns a Response
+     * 
+     * @param array $actionData Data from the Action Controller
+     * @param array $params     Parameters defined in the <result /> block of the 
+     * action
+     * 
+     * @return Response 
+     */
+    public function getResponse(array $actionData = array(), 
+        array $params = array()
+    ) {
+        if (!isset($params['file']) || empty($params['file'])) {
+            throw new Exception(
+                sprintf('Missing template "file" parameter')
+            );
+        } 
+        
+        $file = $params['file'];
+        $twig = $this->getServices()->get($this->params['twigService']);
+        
+        return new Response($twig->render($file, $actionData));
+    }
+    
+    public function getServices()
+    {
+        return $this->services;
+    }
+    
+    public function setServices(Container $container) 
+    {
+        $this->services = $container;
+    }
+}
